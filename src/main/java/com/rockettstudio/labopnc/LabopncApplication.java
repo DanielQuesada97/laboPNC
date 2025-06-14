197bea38ca36c1f089563da21ddbb42595e100ad
DanielQuesada97 <00147619@uca.edu.sv>
2025-06-14 07:33:57 -0600

    first commit

---
+++ src/main/java/com/rockettstudio/labopnc/LabopncApplication.java
@@ -0,0 +1,13 @@
+package com.rockettstudio.labopnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabopncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabopncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockettstudio/labopnc/LabopncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockettstudio.labopnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabopncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
